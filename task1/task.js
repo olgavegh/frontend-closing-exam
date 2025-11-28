1c0cd4595c50f1b3c4cd0cc8029a0c4b6b1b1f50
olgavegh <olgi.vegh@gmail.com>
2025-11-28 11:47:58 +0100

    finish task1 - all test green

--- task1/task.js
+++ task1/task.js
@@ -1,3 +1,25 @@
 export function getLongestBookByAuthorName(authors, books, authorName) {
+  // edge cases
+  if (authors.length === 0 || books.length === 0 || authorName === "") {
+    return null;
+  }
+  // Author
+  const author = authors.find((author) => author.name === authorName);
+  // the author is not found
+  if (!author) return null;
+  const authorId = author.id;
 
+  // Books
+  const authorBooks = books.filter((book) => book.author_id === authorId);
+  const maxPages = Math.max(...authorBooks.map((book) => book.pages));
+  //   console.log(maxPages);
+  const longestBooks = authorBooks.filter((book) => book.pages === maxPages);
+  //   console.log(longestBooks);
+
+  const sortedLongestBooks = longestBooks.sort((a, b) =>
+    a.title.localeCompare(b.title)
+  );
+  //   console.log(sortedLongestBooks);
+
+  return sortedLongestBooks.length ? sortedLongestBooks[0].title : null;
 }
